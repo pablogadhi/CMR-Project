0fb22e1c800b424f9aba71e0e66608238dc300fb
Unknown <pablogadhi@gmail.com>
2018-03-07 21:48:23 -0600

    Admin Site

--- RealECMR/dbconnection/models.py
+++ RealECMR/dbconnection/models.py
@@ -4,4 +4,6 @@ class Client(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
 # Create your models here.
