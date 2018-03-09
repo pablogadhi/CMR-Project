87a4777638916fa7c6572b4d0e8d51a4a5384cb4
Unknown <pablogadhi@gmail.com>
2018-03-09 09:20:31 -0600

    Algunos arreglos temporales de las Entidades

--- RealECMR/dbconnection/models.py
+++ RealECMR/dbconnection/models.py
@@ -1,26 +1,29 @@
 from django.db import models
 
 class Cliente(models.Model):
-    id = models.AutoField()
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=35)
     edad = models.IntegerField(default=0)
     telefono = models.IntegerField(default=0)
     sexo = models.CharField(max_length=10)
     rol = models.CharField(max_length=15)
-    foto = models.ImageField()
+    # foto = models.ImageField()
     fechaInicio = models.DateField()
     activo = models.BooleanField()
     mail = models.EmailField()
+    
+    def __str__(self):
+       return self.nombre
 
 class Propietario(models.Model):
     idP = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    nombreP = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    # nombreP = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     bienPoseido = models.CharField(max_length=20)
     direccion = models.CharField(max_length=50)
 
 class Comprador(models.Model):
     idC = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    nombreC = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    # nombreC = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     zona = models.IntegerField(default=0)
     tipoPropiedad = models.CharField(max_length=20)
     presupuesto = models.FloatField()
@@ -30,15 +33,14 @@ class Propiedad(models.Model):
     valuacion = models.FloatField()
     tipo = models.CharField(max_length=20)
     informacion = models.CharField(max_length=100)
-    fotos = models.ImageField()
-    tamano = models.DecimalField(decimal_places=5, max_digits=3)
+    # fotos = models.ImageField()
+    tamano = models.DecimalField(decimal_places=2, max_digits=8)
 
 class Intermediario(models.Model):
     idI = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    comision = models.DecimalField(decimal_places=6, max_digits=2)
+    comision = models.DecimalField(decimal_places=2, max_digits=8)
     experiencia = models.IntegerField(default=0)
     nombreI = models.CharField(max_length=35)
     
-#    def __str__(self):
-#        return self.name
+ 
 # Create your models here.
