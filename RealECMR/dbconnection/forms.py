21b33551f973d5017c522ef8203bbbae53485bf6
Unknown <pablogadhi@gmail.com>
2018-03-28 13:50:30 -0600

    Custom Forms

--- RealECMR/dbconnection/forms.py
+++ RealECMR/dbconnection/forms.py
@@ -2,16 +2,6 @@ from django import forms
 from django.db import connection
 from .models import CamposAdicionales, Propiedad, Comprador
 
-class QueryForm(forms.Form):
-    query = forms.CharField(label='Query:', max_length=500,widget=forms.TextInput(attrs={'size':'120'}))
-
-    def send(self):
-        data = self.cleaned_data
-        cursor = connection.cursor()
-        cursor.execute(data.get("query"))
-        return cursor.fetchall()
-        # print(data.get("query"))
-
 class AgregarCampoForm(forms.Form):
     nombre = forms.CharField(max_length=50)
     tipo = forms.CharField(max_length=35)
@@ -25,23 +15,109 @@ class AgregarCampoForm(forms.Form):
         # elif data.get("tabla") == "propietario":
         #     tablaNum = 1
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO dbconnection_camposadicionales VALUES (%s, %s, %s, %s)", [id, data.get("nombre"), data.get("tipo"), tablaNum])
+        cursor.execute("INSERT INTO dbconnection_camposadicionales VALUES (%s, %s, %s, %s)", [
+                       id, data.get("nombre"), data.get("tipo"), tablaNum])
 
 
-class AgregarPropiedad(forms.ModelForm):
-    class Meta:
-        model = Propiedad
-        fields = ['direccion', 'valuacion', 'tipo', 'informacion', 'tamano']
+class AgregarPropiedad(forms.Form):
+    propietario = forms.IntegerField(widget=forms.TextInput)
+    intermediario = forms.IntegerField(widget=forms.TextInput)
+    direccion = forms.CharField(max_length=50)
+    valuacion = forms.FloatField(widget=forms.TextInput)
+    TIPOBIEN = (
+        ('Apartamento','Apartamento'),
+        ('Casa','Casa'),
+        ('Terreno','Terreno'),
+        ('Otro','Otro')
+    )
+    tipo = forms.ChoiceField(choices=TIPOBIEN)
+    informacion = forms.CharField(max_length=500)
+    foto = forms.FileField(required=False)
+    tamano = forms.DecimalField(widget=forms.TextInput)
 
     def agregarPropiedad(self):
         foto = None
         data = self.cleaned_data
         id = Propiedad.objects.count()
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO dbconnection_propiedad VALUES (%s, %s, %s, %s, %s, %s, %s)", [id, data.get("direccion"), data.get("valuacion"), data.get("tipo"), data.get("informacion"), foto, data.get("tamano")])
+        cursor.execute("INSERT INTO dbconnection_propiedad VALUES (%s, %s, %s, %s, %s, %s, %s)", [id, data.get(
+            "direccion"), data.get("valuacion"), data.get("tipo"), data.get("informacion"), foto, data.get("tamano")])
 
+class AgregarComprador(forms.Form):
+    activo = forms.BooleanField(initial=True)
+    nombre = forms.CharField(max_length=50)
+    TIPOSEXO = (
+        ('Femenino', 'Femenino'),
+        ('Masculino', 'Masculino'),
+    )
+    sexo = forms.ChoiceField(choices=TIPOSEXO)
+    edad = forms.IntegerField(widget=forms.TextInput)
+    telefono = forms.IntegerField(widget=forms.TextInput)
+    mail = forms.EmailField()
+    cuenta = forms.CharField()
+    fechaInico = forms.DateField(
+        label='Fecha de Inicio', widget=forms.SelectDateWidget)
+    TIPOREP = (
+        ('Normal', 'Normal'),
+        ('Buena', 'Buena'),
+        ('Mala', 'Mala'),
+    )
+    reputacion = forms.ChoiceField(choices=TIPOREP)
+    foto = forms.FileField(required=False)
+    zona = forms.IntegerField()
+    TIPOBIEN = (
+        ('Apartamento', 'Apartamento'),
+        ('Casa', 'Casa'),
+        ('Terreno', 'Terreno'),
+        ('Otro', 'Otro')
+    )
+    tipoPropiedad = forms.ChoiceField(
+        choices=TIPOBIEN, label='Tipo de Propiedad')
+    presupuesto = forms.FloatField(widget=forms.TextInput)
 
-class AgregarComprador(forms.ModelForm):
-    class Meta:
-        model = Comprador
-        fields = ['activo', 'nombre', 'sexo', 'edad', 'telefono', 'mail', 'fechaInicio', 'reputacion', 'zona', 'tipoPropiedad', 'presupuesto']
+class AgregarPropietario(forms.Form):
+    activo = forms.BooleanField(initial=True)
+    nombre = forms.CharField(max_length=50)
+    TIPOSEXO = (
+        ('Femenino', 'Femenino'),
+        ('Masculino', 'Masculino'),
+    )
+    sexo = forms.ChoiceField(choices=TIPOSEXO)
+    edad = forms.IntegerField(widget=forms.TextInput)
+    telefono = forms.IntegerField(widget=forms.TextInput)
+    mail = forms.EmailField()
+    cuenta = forms.CharField()
+    fechaInico = forms.DateField(
+        label='Fecha de Inicio', widget=forms.SelectDateWidget)
+    TIPOREP = (
+        ('Normal', 'Normal'),
+        ('Buena', 'Buena'),
+        ('Mala', 'Mala'),
+    )
+    reputacion = forms.ChoiceField(choices=TIPOREP)
+    foto = forms.FileField(required=False)
+    direccion = forms.CharField(max_length=50)
+
+class AgregarIntermediario(forms.Form):
+    activo = forms.BooleanField(initial=True)
+    nombre = forms.CharField(max_length=50)
+    TIPOSEXO = (
+        ('Femenino', 'Femenino'),
+        ('Masculino', 'Masculino'),
+    )
+    sexo = forms.ChoiceField(choices=TIPOSEXO)
+    edad = forms.IntegerField(widget=forms.TextInput)
+    telefono = forms.IntegerField(widget=forms.TextInput)
+    mail = forms.EmailField()
+    cuenta = forms.CharField()
+    fechaInico = forms.DateField(
+        label='Fecha de Inicio', widget=forms.SelectDateWidget)
+    TIPOREP = (
+        ('Normal', 'Normal'),
+        ('Buena', 'Buena'),
+        ('Mala', 'Mala'),
+    )
+    reputacion = forms.ChoiceField(choices=TIPOREP)
+    foto = forms.FileField(required=False)
+    comision = forms.DecimalField(widget=forms.TextInput)
+    experiencia = forms.IntegerField(widget=forms.TextInput)
